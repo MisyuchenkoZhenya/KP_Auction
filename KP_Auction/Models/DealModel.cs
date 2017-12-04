4dac26d6ac1171256d33d69580271c9619c672d4
Zhenya <misyuchenko.zhenya@mail.ru>
2017-12-05 01:53:56 +0300

    create empty models for future

---
+++ KP_Auction/Models/DealModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace KP_Auction.Models
+{
+    public class DealModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int Buyer_Id { get; set; }
+
+        [Required]
+        public int Seller_Id { get; set; }
+
+        [Required]
+        public int Item_Id { get; set; }
+
+        [Required]
+        public int Auction_Id { get; set; }
+
+        [Required]
+        public int DealState_Id { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:g}", ApplyFormatInEditMode = true)]
+        public DateTime Time { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+    }
+}
---
+++ KP_Auction/Repositories/ItemCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KP_Auction.Repositories
+{
+    public class ItemCategoryRepository
+    {
+    }
+}
